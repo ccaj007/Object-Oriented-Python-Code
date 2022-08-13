d3037c7eb4331081897354a4662fcb159c6e4507
C L <ccaj007@gmail.com>
2022-08-12 20:36:38 -0400

    test

--- 1-HigherLower.py
+++ 1-HigherLower.py
@@ -11,7 +11,7 @@ def getCard(deckListIn):
     thisCard = deckListIn.pop()
     return thisCard
 
-def suffle(deckListIn):
+def shuffle(deckListIn):
     deckListOut =  deckListIn.copy()
     random.shuffle(deckListOut)
     return deckListOut
@@ -35,8 +35,50 @@ for suit in SUIT_TUPLE:
 score = 50
 
 while True:
-    print()
     gameDeckList = shuffle(startingDeckList)
+    currentCardDict = getCard(gameDeckList)
+    currentCardRank = currentCardDict['rank']
+    currentCardValue = currentCardDict['value']
+    currentCardSuit = currentCardDict['suit']
+    print(f'\nStarting card is: {currentCardRank} of {currentCardSuit}\n')
+
+    for cardNumber in range (0,NCARDS):
+        answer = input(f'Will the next card be higher or lower than the {currentCardRank} of {currentCardSuit}? (enter h or l)')
+        answer = answer.lower()
+
+        nextCardDict = getCard(gameDeckList)
+        nextCardRank = nextCardDict['rank']
+        nextCardValue = nextCardDict['value']
+        nextCardSuit = nextCardDict['suit']
+        print(f'\nNext card is: {nextCardRank} of {nextCardSuit}\n')
+
+        if answer == 'h':
+            if nextCardValue > currentCardValue:
+                print('You guessed correctly, it was higher')
+                score += 20
+            else:
+                print('Sorry it was not higher')
+                score -=15
+
+        elif answer == 'l':
+            if nextCardValue < currentCardValue:
+                print('You guessed correctly, it was lower')
+                score += 20
+            else:
+                print('Sorry it was not lower')
+                score -=15
+        else:
+            print('\nYou have to guess h or l...')
+
+        print(f'Your score is: {score}\n')
+        currentCardRank = nextCardRank
+        currentCardValue = nextCardValue
+
+        goAgain = input('To play again, press ENTER, or "q" to quit: ')
+        if goAgain.lower() == 'q':
+            break
+print('OK bye')
+
 
 
 
